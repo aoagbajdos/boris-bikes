a32a16ed6f64529b38413586cfe2a2c3c7597052
Richard <aagbajdos@gmail.com>
2019-10-30 14:29:36 +0000

    feature_testing

--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -1,15 +1,15 @@
 require './lib/boris-bikes'
 
 describe DockingStation do
-  it 'creates DockingStation' do
+  #it 'creates DockingStation' do
   dockingstation = DockingStation.new
 end
 
-it 'releases bike' do
-  dockingstation = DockingStation.new
-  expect(dockingstation).to respond_to(:release_bike)
-end
-it 'bike'
-  release_bike = Bike.new
-  expect(Bike).to respond_to(:working?)
-end
+# it 'releases bike' do
+#   dockingstation = DockingStation.new
+#   expect(dockingstation).to respond_to(:release_bike)
+# end
+# it 'bike'
+#   release_bike = Bike.new
+#   expect(Bike).to respond_to(:working?)
+# end
